330dba11c40f6d9130c168ae2511100bbf9f62ea
KOLIYAR NIKHIL DURAIRAJ <koliyarnikhil@gmail.com>
2025-11-01 12:05:50 +0000

    lets now work on the dedicated Contact tab, In the Contact tab display t

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,15 @@
+"use client";
 
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import ContactSection from "@/components/sections/contact";
 
 export default function ContactPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold">Contact</h1>
-        {/* Page content will go here */}
+      <main className="flex-1">
+        <ContactSection />
       </main>
       <Footer />
     </div>
