fb67644950658f4730fce6916d104a3250f0f1fb
KOLIYAR NIKHIL DURAIRAJ <koliyarnikhil@gmail.com>
2025-11-01 11:52:14 +0000

    lets now work on the dedicated Projects tab, In the Projects tab display

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,14 @@
 
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import ProjectsSection from "@/components/sections/projects";
 
 export default function ProjectsPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold">Projects</h1>
-        {/* Page content will go here */}
+      <main className="flex-1">
+        <ProjectsSection />
       </main>
       <Footer />
     </div>
