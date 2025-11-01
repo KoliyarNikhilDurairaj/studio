27fa8b91689ff56ec5f5c3db49213347d0c7db3c
KOLIYAR NIKHIL DURAIRAJ <koliyarnikhil@gmail.com>
2025-11-01 11:59:35 +0000

    lets now work on the dedicated Goals tab, In the Goals tab display the s

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -1,14 +1,15 @@
+"use client";
 
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import GoalsSection from "@/components/sections/goals";
 
 export default function GoalsPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold">Goals</h1>
-        {/* Page content will go here */}
+      <main className="flex-1">
+        <GoalsSection />
       </main>
       <Footer />
     </div>
