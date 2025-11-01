2708074c2a76a62ee9711f593f9f3478ef5fe36c
KOLIYAR NIKHIL DURAIRAJ <koliyarnikhil@gmail.com>
2025-11-01 12:04:28 +0000

    lets now work on the dedicated Connect tab, In the Connect tab display t

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,14 +1,15 @@
+"use client";
 
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import SocialSection from "@/components/sections/social";
 
 export default function ConnectPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold">Connect</h1>
-        {/* Page content will go here */}
+      <main className="flex-1">
+        <SocialSection />
       </main>
       <Footer />
     </div>
