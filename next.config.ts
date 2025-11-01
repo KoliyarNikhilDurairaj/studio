d5e65163e9825c6e292a9ffad90bb19ff0842a62
KOLIYAR NIKHIL DURAIRAJ <koliyarnikhil@gmail.com>
2025-11-01 09:56:08 +0000

    1. Add “output: 'export'” to next.config.ts so as to resolve the issue “

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -24,7 +25,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
