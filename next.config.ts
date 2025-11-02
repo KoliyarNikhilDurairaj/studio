b54789ef77d6096f2e789d0dfb6762febe9dd81a
KOLIYAR NIKHIL DURAIRAJ <koliyarnikhil@gmail.com>
2025-11-02 12:10:07 +0000

    1. Add “output: 'export'” to next.config.ts so as to resolve the issue “

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,7 +32,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,',
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
